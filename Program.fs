ac49ec0ad0ba50e867513763b7c164e0a82fc8c8
Marsello <marcel@cct.lsu.edu>
2020-02-25 14:56:00 +0100

    better name

--- Program.fs
+++ Program.fs
@@ -6,15 +6,6 @@ let main argv =
     Wav.readAllWav "E:/2020/Audio/MesoTest.wav"
     |> Wav.dFourierL 8192 32
     |> createHeatModel (-1.0) (1.0) ( 1.0 ) 
-    |> showChartAndRun "Spectro Test"
+    |> showChartAndRun "Spectrogram"
 
     0 // return an integer exit code
-
-
-    //Playground.Akima()
-    //Playground.DFT()
-    //Playground.Halton()
-    //Playground.Harmonic()
-
-    //Playground.DFTWavCompact()
-
