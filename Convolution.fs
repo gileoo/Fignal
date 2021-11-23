4cbf31926e049bed6e59367204506f03111a089e
Marcel <marcel@cct.lsu.edu>
2021-11-23 10:03:36 +0100

    short idx name

--- Convolution.fs
+++ Convolution.fs
@@ -2,33 +2,14 @@
 
 module Discrete =
 
-    let conv (x:float[]) (k:float[]) =
-        Array.zeroCreate (x.Length + (k.Length-1))
-        |> Array.mapi( fun i _ -> 
-            k
-            |> Array.mapi( fun j ke -> 
-                let xIdx = i + j - (k.Length-1) 
-                if xIdx >= 0 && xIdx < x.Length then
-                    ke * x.[xIdx]
-                else
-                    0.0 )
-            |> Array.sum )
-
-
-    let longDiv (x:float[]) (k:float[]) =
-    
-        let y = Array.zeroCreate x.Length
-        y.[0] <- x.[0]
-
-        [| 0 .. x.Length-2 |]
-        |> Array.iter( fun i ->
-            let d   = y.[i] / k.[0]
-            [| 1 .. k.Length-1 |]
-            |> Array.iter( fun j-> 
-                if i+j < y.Length-1 then
-                    y.[i+j] <- x.[i+j] - d * k.[j] ) 
-            y.[i] <- d
-            //printfn "i: %d, d: %.3f, x[i]: %.3f ,y=%A" i d x.[i] y
-            )
-
-        (y, x)
+let convolveDiscrete (x:float[]) (k:float[]) =
+    Array.zeroCreate (x.Length + (k.Length-1))
+    |> Array.mapi( fun i _ -> 
+        k
+        |> Array.mapi( fun j ke -> 
+            let ix = i + j - (k.Length-1) 
+            if ix >= 0 && ix < x.Length then
+                ke * x.[ix]
+            else
+                0.0 )
+        |> Array.sum )
